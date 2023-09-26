3b592453dd2b91b332957777edb85267074f9066
Edwin Chukwuebuka <edwinchukwuebukae@gmail.com>
2023-09-26 23:32:20 +0100

    url configuration and testing

--- api/models.py
+++ api/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Sneaker(models.Model):
-    title = models.CharField()
-    description = models.CharField()
+    title = models.CharField(max_length=32)
+    description = models.CharField(max_length=128)
     price = models.IntegerField()
     image = models.ImageField()
 
     def __str__(self):
         return self.title
-    
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-# Create your views here.
+from django.http import JsonResponse
+from api.models import Sneaker
 
+# Create your views here.
+def get_sneakers(request):
+    sneakers = Sneaker.objects.all()
+    sneakers_python = list(sneakers.values())
+    return JsonResponse(
+        {
+        'sneakers': sneakers_python
+        }
+    )
