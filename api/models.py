08cced4df106e3bb0a1b8b5dbfd1d3c16b854ed8
Edwin Chukwuebuka <edwinchukwuebukae@gmail.com>
2023-09-26 23:00:33 +0100

    created sneaker model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Sneaker(models.Model):
+    title = models.CharField()
+    description = models.CharField()
+    price = models.IntegerField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
+    
